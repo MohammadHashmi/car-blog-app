01598a75b09cd3f1be75a65b59fd92bf5a496cab
Dubi-1 <60275618+MohammadHashmi@users.noreply.github.com>
2024-09-10 16:49:42 -0600

    THANK YOU SO MUCH HEADLESSUI

---
+++ client/client/src/components/Modal.tsx
@@ -0,0 +1,60 @@
+import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+import { useState } from "react";
+
+export default function Modal() {
+  let [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <button onClick={() => setIsOpen(true)}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="white"
+          className="size-10 "
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z"
+          />
+        </svg>
+      </button>
+      <Dialog
+        open={isOpen}
+        as="div"
+        className="relative z-10 focus:outline-none"
+        onClose={() => setIsOpen(false)}
+      >
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4">
+            <DialogPanel
+              transition
+              className="w-full max-w-md rounded-xl bg-white/5 p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
+            >
+              <DialogTitle
+                as="h3"
+                className="text-base/7 font-medium text-white"
+              >
+                How It Works
+              </DialogTitle>
+              <p className="mt-2 text-sm/6 text-white/50">
+                Your payment has been successfully submitted. We’ve sent you an
+                email with all of the details of your order.
+              </p>
+              <div className="mt-4">
+                <button
+                  className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[focus]:outline-1 data-[focus]:outline-white data-[open]:bg-gray-700"
+                  onClick={() => setIsOpen(false)}
+                >
+                  Got it, thanks!
+                </button>
+              </div>
+            </DialogPanel>
+          </div>
+        </div>
+      </Dialog>
+    </>
+  );
+}
--- client/client/src/pages/Write.tsx
+++ client/client/src/pages/Write.tsx
@@ -1,6 +1,7 @@
 import MDEditor from "@uiw/react-md-editor";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import Modal from "../components/Modal";
 
 export default function Write() {
   const [value, setValue] = useState<any | null>(
@@ -18,9 +19,10 @@ enter your own text here
           className=""
           value={value}
           visibleDragbar={false}
-          height={400}
+          height={450}
           onChange={(string) => setValue(string)}
         />
+
         <div className="flex justify-center">
           <button className="text-white font-bold bg-transparent border border-white ease-in duration-200 hover:bg-white hover:text-black py-2 px-4 rounded m-2">
             Submit Article
@@ -30,23 +32,7 @@ enter your own text here
               Return Home
             </button>
           </Link>
-
-          <button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="white"
-              className="size-10 "
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z"
-              />
-            </svg>
-          </button>
+          <Modal />
         </div>
       </div>
     </>
